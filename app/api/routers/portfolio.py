f93f62216138f4729b82b424688cc6be4185898d
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-10-13 21:02:51 +0100

    fixed auth

--- app/api/routers/portfolio.py
+++ app/api/routers/portfolio.py
@@ -2,16 +2,18 @@
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette.requests import Request
+
 from app.database import get_db
-from app.api.routers.auth import get_current_user
 from app.models import Portfolio, User
 
 router = APIRouter()
 
 @router.get("/balance")
-def get_portfolio_balance(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def get_portfolio_balance(request : Request, db: Session = Depends(get_db)):
     # Query the user's portfolio
-    portfolio = db.query(Portfolio).filter(Portfolio.user_id == user.id).first()
+    id = db.query(User).filter(User.username == request.state.user).first().id
+    portfolio = db.query(Portfolio).filter(Portfolio.user_id == id).first()
     if not portfolio:
         raise HTTPException(status_code=404, detail="Portfolio not found")
 
--- app/middleware/auth_middleware.py
+++ app/middleware/auth_middleware.py
@@ -1,21 +1,28 @@
 # Authentication middleware
 from fastapi import Request, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
-from jose import jwt
+from jose import jwt , JWTError
 from app.config import settings
 
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        # Skip authentication for root ("/"), auth routes ("/auth"), and OpenAPI docs
+        if request.url.path in ["/", "/docs", "/redoc", "/openapi.json"] or request.url.path.startswith("/auth"):
+            return await call_next(request)
+
         token = request.headers.get('Authorization')
-        if token is None:
+        if token is None or not token.startswith('Bearer '):
             raise HTTPException(status_code=403, detail="Not authorized")
 
         try:
+            token = token.split(" ")[1]
             payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
-            request.state.user = payload.get('sub')
-        except jwt.JWTError:
+            request.state.user = payload.get("sub")
+        except (JWTError, IndexError):
+            print('error log',JWTError)
             raise HTTPException(status_code=403, detail="Invalid token")
 
         response = await call_next(request)
         return response
+
